9e5f4013ca7e0a068ddaf62f4467bef6c655e405
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2022-12-30 15:44:21 -0500

    Implementing JWT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.json('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +17,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
