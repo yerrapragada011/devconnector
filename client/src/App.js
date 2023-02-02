64484d6c4de5ffd7e7a8fbd9b312579a5b42d5b9
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-02 16:47:26 -0500

    Alert Component & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -14,11 +15,14 @@ const App = () => (
   <Provider store={store}>
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+      <section className='container'>
+        <Alert />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </section>
     </Router>
   </Provider>
 );
