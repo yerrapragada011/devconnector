f8a423e012b3f1fb8121537b0fe81bd7ac67ebe7
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-06 13:10:44 -0500

    Protected Route For Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -26,14 +28,16 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Navbar />
-        <section className='container'>
-          <Alert />
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </section>
+        <Alert />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route
+            path='/dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
+        </Routes>
       </Router>
     </Provider>
   );
