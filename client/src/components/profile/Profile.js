58a863ec660e87bee014dcbd72f5eb984a42c218
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-13 16:26:37 -0500

    ProfileTop & ProfileAbout Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import { Link, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile }, auth, loading }) => {
@@ -29,6 +31,10 @@ const Profile = ({ getProfileById, profile: { profile }, auth, loading }) => {
                 Edit Profile{' '}
               </Link>
             )}
+          <div class='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -16,7 +16,7 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
         <Spinner />
       ) : (
         <Fragment>
-          <h1 className='large text-primary'>Devlopers</h1>
+          <h1 className='large text-primary'>Developers</h1>
           <p className='lead'>
             <i className='fab fa-connectdevelop'></i> Browse and connect with
             developers
