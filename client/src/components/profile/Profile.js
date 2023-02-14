bc696488915a585b78bb21717ddc34420098a10d
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-14 14:08:53 -0500

    Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile }, auth }) => {
@@ -66,6 +67,9 @@ const Profile = ({ getProfileById, profile: { profile }, auth }) => {
               )}
             </div>
 
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
