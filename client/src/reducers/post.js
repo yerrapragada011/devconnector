bc06b97c6219c4c2dc83a0b6805d7d7dfe31a124
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-17 13:45:21 -0500

    Adding Posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,10 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from '../actions/types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,12 +23,18 @@ export default function posts(state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false,
+      };
     case DELETE_POST:
       return {
         ...state,
-        posts: state.posts.filter(post => post._id !== payload),
-        loading: false
-      }
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
