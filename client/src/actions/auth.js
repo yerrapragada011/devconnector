b0d2283586d0d754e64da10cdf10ecae993c0c3a
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-02-20 16:45:44 -0500

    Register & Login fix

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,18 +8,12 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_PROFILE,
 } from './types';
-import setAuthToken from '../utils/setAuthToken';
 
 // Load User
 export const loadUser = () => async (dispatch) => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
-
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get('/auth');
 
     dispatch({
       type: USER_LOADED,
@@ -33,50 +27,35 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register =
-  ({ name, email, password }) =>
-  async (dispatch) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+export const register = (formData) => async (dispatch) => {
+  try {
+    const res = await axios.post('/users', formData);
 
-    const body = JSON.stringify({ name, email, password });
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
 
-    try {
-      const res = await axios.post('/api/users', body, config);
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: res.data,
-      });
-
-      dispatch(loadUser());
-    } catch (err) {
-      const errors = err.response.data.errors;
-
-      if (errors) {
-        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-      }
-
-      dispatch({
-        type: REGISTER_FAIL,
-      });
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
-  };
+
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
 
 // Login User
 export const login = (email, password) => async (dispatch) => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const body = JSON.stringify({ email, password });
+  const body = { email, password };
 
   try {
-    const res = await axios.post('/api/auth', body, config);
+    const res = await axios.post('/auth', body);
+
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data,
@@ -96,9 +75,5 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-// Logout / Clear Profile
-
-export const logout = () => (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE });
-  dispatch({ type: LOGOUT });
-};
+// Logout
+export const logout = () => ({ type: LOGOUT });
--- routes/api/users.js
+++ routes/api/users.js
@@ -33,7 +33,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exits ' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       const avatar = gravatar.url(email, {
